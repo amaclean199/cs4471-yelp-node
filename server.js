e2a7ab33f92795cbe051066b101cc5f0ef745979
Vince Sippola <Vince Sippola>
2017-03-13 16:15:12 -0400

    tset

--- server.js
+++ server.js
@@ -152,7 +152,7 @@ app.get("/reviews2", function(request, response) {
   // and we call on the connection to return us all the documents in the
   // words collection.
   var x = JSON.parse('funny:{$gt:83}');
-  response.send(x);
+  response.send(JSON.stringify(X));
   // mongodb.collection("reviews").find().toArray(function(err, words) {
   //   if (err) {
   //    response.status(500).send(err);
