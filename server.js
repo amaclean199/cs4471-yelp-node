96e19f54cf20440a380e152721527160a1a3b5d9
Amha Tsegaye <amha_ts@hotmail.com>
2017-03-13 10:04:08 -0400

    added parameter

--- server.js
+++ server.js
@@ -133,11 +133,24 @@ app.get("/words", function(request, response) {
   });
 });
 
+// // Then we create a route to handle our example database call
+// app.get("/reviews", function(request, response) {
+//   // and we call on the connection to return us all the documents in the
+//   // words collection.
+//   mongodb.collection("reviews").find().toArray(function(err, words) {
+//     if (err) {
+//      response.status(500).send(err);
+//     } else {
+//      response.send(words);
+//     }
+//   });
+// });
+
 // Then we create a route to handle our example database call
-app.get("/reviews", function(request, response) {
+app.get("/reviews/:param", function(request, response) {
   // and we call on the connection to return us all the documents in the
   // words collection.
-  mongodb.collection("reviews").find().toArray(function(err, words) {
+  mongodb.collection(request.param).find().toArray(function(err, words) {
     if (err) {
      response.status(500).send(err);
     } else {
@@ -145,7 +158,6 @@ app.get("/reviews", function(request, response) {
     }
   });
 });
-
 
 // Now we go and listen for a connection.
 app.listen(port);
