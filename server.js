7247150049d148eff54df52cda90678e6d074064
Amha Tsegaye <amha_ts@hotmail.com>
2017-03-13 11:20:13 -0400

    added version

--- server.js
+++ server.js
@@ -147,7 +147,7 @@ app.put("/reviews",  function(request, response) {
 // });
 
 // Then we create a route to handle our example database call
-app.get("/api/:values", function(request, response) {
+app.get("/api/data/:values", function(request, response) {
   // and we call on the connection to return us all the documents in the
   // words collection.
   mongodb.collection(request.params.values).find().toArray(function(err, words) {
