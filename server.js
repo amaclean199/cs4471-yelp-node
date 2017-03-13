9c83c79a1d6d9923abf3e55b4468f2b0a1a40c00
Vince Sippola <Vince Sippola>
2017-03-13 16:45:33 -0400

    test

--- server.js
+++ server.js
@@ -151,22 +151,19 @@ app.get("/reviews", function(request, response) {
 app.get("/reviews2", function(request, response) {
   // and we call on the connection to return us all the documents in the
   // words collection.\
-  var text = '{ "employees" : [' +
-'{ "firstName":"John" , "lastName":"Doe" },' +
-'{ "firstName":"Anna" , "lastName":"Smith" },' +
-'{ "firstName":"Peter" , "lastName":"Jones" } ]}';
+  var text = '{ "funny" : "83" }';
 
   var obj = JSON.parse(text);
   var myJSON = JSON.stringify(myObj);
-  //var x = JSON.parse();
+
   response.send(myJSON);
-  // mongodb.collection("reviews").find().toArray(function(err, words) {
-  //   if (err) {
-  //    response.status(500).send(err);
-  //   } else {
-  //    response.send(words);
-  //   }
-  // });
+  mongodb.collection("reviews").find().toArray(function(err, words) {
+    if (err) {
+     response.status(500).send(err);
+    } else {
+     response.send(words);
+    }
+  });
 });
 
 // Then we create a route to handle our example database call
