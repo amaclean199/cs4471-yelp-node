8980c8a619e09ab883fd37a1783bcf0bf0c462ca
Amha Tsegaye <amha_ts@hotmail.com>
2017-03-13 15:46:51 -0400

    review

--- server.js
+++ server.js
@@ -138,9 +138,8 @@ app.get("/api/data/:values", function(request, response) {
 app.get("/reviews", function(request, response) {
   // and we call on the connection to return us all the documents in the
   // words collection.
-  {{}}
-  var x = JSON.parse("\"funny\":{\"$gt\":\"83\"}");
-  mongodb.collection("reviews").find(x).toArray(function(err, words) {
+  //var x = JSON.parse("\"funny\":{\"$gt\":\"83\"}");
+  mongodb.collection("reviews").find().toArray(function(err, words) {
     if (err) {
      response.status(500).send(err);
     } else {
