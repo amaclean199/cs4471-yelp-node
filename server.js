62706babc316c9de87a5cb7876ea7e540bd138f7
Amha Tsegaye <amha_ts@hotmail.com>
2017-03-13 12:16:47 -0400

    parameter postion

--- server.js
+++ server.js
@@ -124,7 +124,7 @@ app.put("/reviews",  function(request, response) {
 app.get("/api/data/:values", function(request, response) {
   // and we call on the connection to return us all the documents in the
   // words collection.
-  mongodb.collection("reviews").find({request.params.values: "83"}).toArray(function(err, words) {
+  mongodb.collection("reviews").find().toArray(function(err, words) {
     if (err) {
      response.status(500).send(err);
     } else {
