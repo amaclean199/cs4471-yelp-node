2ea1263bca20d0a7230fc4c9a1f1ca82f2d643b1
vsippola <vsippola@uwo.ca>
2017-03-13 21:28:09 -0400

    updating database

--- server.js
+++ server.js
@@ -132,7 +132,9 @@ app.put("/reviews",  function(request, response) {
 app.get("/reviews", function(request, response) {
   // and we call on the connection to return us all the documents in the
   // words collection.
-  mongodb.collection("yelp").find().toArray(function(err, words) {
+
+  var x = {funny: {$gt:74}};
+  mongodb.collection("yelp").find(x).toArray(function(err, words) {
     if (err) {
      response.status(500).send(err);
     } else {
