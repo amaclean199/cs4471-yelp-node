d77c8e04b73421156f886fc3de8f9f94caaa1eb9
vsippola <vsippola@uwo.ca>
2017-03-13 21:44:03 -0400

    updating database

--- server.js
+++ server.js
@@ -132,10 +132,7 @@ app.put("/reviews",  function(request, response) {
 app.get("/reviews", function(request, response) {
   // and we call on the connection to return us all the documents in the
   // words collection.
-
-  var s = {"funny": {"$gt": 74}};
-  var j = JSON.parse(s)
-  mongodb.collection("yelp").find(s).toArray(function(err, words) {
+  mongodb.collection("yelp").find().toArray(function(err, words) {
     if (err) {
      response.status(500).send(err);
     } else {
