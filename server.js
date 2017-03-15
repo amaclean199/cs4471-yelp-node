f9eee8a757b0ebce04341567faa518b1e65b2eef
vsippola <vsippola@uwo.ca>
2017-03-14 23:04:18 -0400

    adding search by author to api

--- server.js
+++ server.js
@@ -194,7 +194,7 @@ app.get("/api/v1/authors", function(request, response) {
   //
   // }
 
-  var s = '{"user_id":"wnzfuir72IZFg5RAPOwWCQ"}';
+  var s = '{"user_id":"wnzfuir72IZFg5RAPOwWCQ", "date":"2013-11-17","stars":1}';
   var j = JSON.parse(s);
 
   mongodb.collection("yelp").find(j).toArray(function(err, words) {
@@ -236,24 +236,32 @@ require("cf-deployment-tracker-client").track();
 function isValidDate(dateString)
 {
     // First check for the pattern
-    if(!/^\d{4}\-\d{2}\-\d{2}$/.test(dateString))
+    var regex_date = /^\d{4}\-\d{2}\-\d{2}$/;
+
+    if(!regex_date.test(dateString))
+    {
         return false;
+    }
 
     // Parse the date parts to integers
-    var parts = dateString.split("-");
-    var day = parseInt(parts[2], 10);
-    var month = parseInt(parts[1], 10);
-    var year = parseInt(parts[0], 10);
+    var parts   = dateString.split("-");
+    var day     = parseInt(parts[2], 10);
+    var month   = parseInt(parts[1], 10);
+    var year    = parseInt(parts[0], 10);
 
     // Check the ranges of month and year
     if(year < 1000 || year > 3000 || month == 0 || month > 12)
+    {
         return false;
+    }
 
     var monthLength = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
 
     // Adjust for leap years
     if(year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+    {
         monthLength[1] = 29;
+    }
 
     // Check the range of the day
     return day > 0 && day <= monthLength[month - 1];
